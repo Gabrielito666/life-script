40fa8f2bf16d2794f34417a42172af8c8b3b7d4d
Gabrielito666 <galileo.fandango@gmail.com>
2026-01-10 12:26:35 -0300

    refactor: class Command y Program for order in code

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-const diagnostic = require("./lib/diagnostic");
-const addCalendarDays = require("./lib/add-calendar-days");
-const archiveCalendar = require("./lib/archive-calendar");
-const pushRecurring = require("./lib/push-recurring");
-const fs = require("fs");
+const Program = require("./lib/program");
+const DiagnosticCommand = require("./lib/commands/diagnostic");
+const AddCalendarDaysCommand = require("./lib/commands/add-calendar-days");
+const ArchiveCalendarCommand = require("./lib/commands/archive-calendar");
+const PushRecurringCommand = require("./lib/commands/push-recurring");
 
-const main = async() =>
-{
-	if(!fs.existsSync(process.env.LIFE_DIR))
-	{
-		return diagnostic.logCommandError("~/life dir not exists, create it with mkdir");
-	}
-	if(process.argv[2] === "diagnostic")
-	{
-		
-		if(process.argv.length !== 3) return diagnostic.logCommandError("This command does not require any arguments.");
-		return diagnostic.printErrorOrCongratulations(diagnostic());
-	}
-	if(process.argv[2] === "add-calendar-days")
-	{
-		const numberOfDays = +process.argv[3];
-		if(typeof numberOfDays !== "number" || Number.isNaN(numberOfDays) || process.argv.length > 4)
-		{
-			return diagnostic.logCommandError("The “add-calendar-days” command can only receive a numeric argument.");
-		}
-		return diagnostic.printErrorIfIsError(await addCalendarDays(numberOfDays));
-	}
-	if(process.argv[2] === "archive-calendar")
-	{
-		if(process.argv.length !== 3) return diagnostic.logCommandError("This command does not require any arguments.");
-		return diagnostic.printErrorIfIsError(await archiveCalendar());
-	}
-	if(process.argv[2] === "push-recurring")
-	{
-		
-		if(process.argv.length !== 3) return diagnostic.logCommandError("This command does not require any arguments.");
-		return diagnostic.printErrorIfIsError(await pushRecurring());
-	}
-};
+const program = new Program(
+	new DiagnosticCommand(),
+	new AddCalendarDaysCommand(),
+	new ArchiveCalendarCommand(),
+	new PushRecurringCommand()
+);
 
-main();
+program.run();
